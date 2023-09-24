0e9dc793c56b7605e73b71afa5284bbd552695e9
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-09-24 21:41:28 +0000

    item関連

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,16 +1,18 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
-  
+
   def create
     item = Item.new(item_params)
-    # item.customer_id = customer.id
+    # item.admin_id = admin.id
     if item.save
       redirect_to admin_items_path
     else
@@ -20,14 +22,28 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    # item.admin_id = admin.id
+    if @item.update(item_params)
+      redirect_to admin_item_path
+    else
+      render :edit
+    end
+  end
+
   private
-  
+
   def item_params
-    params.require(:item).permit(:image, :name, :introduction, :price)
+    params.require(:item).permit(:item_image, :name, :introduction, :price, :genre_id, :is_active)
   end
+
 end
