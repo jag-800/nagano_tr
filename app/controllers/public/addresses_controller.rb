3d39ef329147781711b06cc07a49e37180169fff
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-10-03 04:26:12 +0000

    editページ address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -10,31 +10,31 @@ class Public::AddressesController < ApplicationController
     address.save
     redirect_to addresses_path
   end
-  
+
   def edit
-    @address = address.find(params[:id])
+    @address = Address.find(params[:id])
   end
-  
+
   def update
-    address = Address.new(address_params)
-    if address.update
+    address = Address.find(params[:id])
+    if address.update(address_params)
       redirect_to addresses_path
     else
       @address = address.find(params[:id])
       render :edit
     end
   end
-  
+
   def destroy
     address = address.find(params[:id])
     address.destroy
     redirect_to addresses_path
   end
-  
+
   private
 
   def address_params
     params.require(:address).permit(:name, :post_code, :address)
   end
-  
+
 end
